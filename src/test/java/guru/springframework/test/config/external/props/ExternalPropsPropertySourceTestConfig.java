bd3faab343da9d7ab2281e54662f030c45dfeb09
gustavogois <gustavo.gois@gmail.com>
2019-04-04 10:56:20 +0100

    Property Source

---
+++ src/test/java/guru/springframework/test/config/external/props/ExternalPropsPropertySourceTestConfig.java
@@ -0,0 +1,43 @@
+package guru.springframework.test.config.external.props;
+
+import guru.springframework.test.jms.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:testing.properties")
+public class ExternalPropsPropertySourceTestConfig {
+
+    @Value("${guru.jms.server}")
+    String jmsServer;
+
+    @Value("${guru.jms.port}")
+    Integer jmsPort;
+
+    @Value("${guru.jms.user}")
+    String jmsUser;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+                new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUrl(this.jmsServer);
+        fakeJmsBroker.setPort(this.jmsPort);
+        fakeJmsBroker.setUser(this.jmsUser);
+        fakeJmsBroker.setPassword(this.jmsPassword);
+        return fakeJmsBroker;
+    }
+
+}
